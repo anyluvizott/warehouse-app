0b104dc869ad95701e3bc179c2fe17327f3bcfee
Anyelly Marrye Luvizott <any.luvizott@gmail.com>
2022-09-15 17:59:18 -0300

    Iniciando - Listando Galpões

---
+++ spec/system/user_view_homepage_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+describe "Usuario visita tela inicial" do
+  it "e vê o nome da app" do
+    #Arrange - unico bloco que pode estar vazio
+
+    #Act - método visit
+    visit("/")
+
+    #Assert - vai estar sempre cheio de expectativas
+    expect(page).to have_content("Galpões & Estoque")
+  end
+
+  it "e vê os galpões cadastrados" do
+    #Arrange
+    #cadastrar 2 galpoes rio e maceio
+    Warehouse.create(name: "Rio", code: "SDU", city: "Rio de Janeiro", area: 60_000)
+    Warehouse.create(name: "Maceio", code: "MCZ", city: "Maceio", area: 50_000)
+
+    #Act
+    visit("/")
+
+    #Assert
+    expect(page).not_to have_content('Não existem galpões cadastrados')
+    expect(page).to have_content("Rio")
+    expect(page).to have_content("Código: SDU")
+    expect(page).to have_content("Cidade: Rio de Janeiro")
+    expect(page).to have_content("60000 m2")
+
+    expect(page).to have_content("Maceio")
+    expect(page).to have_content("Código: MCZ")
+    expect(page).to have_content("Cidade: Maceio")
+    expect(page).to have_content("50000 m2")
+  end
+
+  it "e não existem galpões cadastrados" do
+    #Arrange
+
+    #Act
+    visit('/')
+
+    #Assert
+    expect(page).to have_content('Não existem galpões cadastrados')
+
+  end
+end
