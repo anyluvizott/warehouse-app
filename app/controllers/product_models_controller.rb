9c9256deaf2efff259628906be82c7acee9325a5
Anyelly Marrye Luvizott <any.luvizott@gmail.com>
2022-09-26 20:24:07 -0300

    Modelos de Produtos

--- app/controllers/product_models_controller.rb
+++ app/controllers/product_models_controller.rb
@@ -1,7 +1,27 @@
 class ProductModelsController < ApplicationController
-  
   def index
     @product_models = ProductModel.all
   end
 
+  def new
+    @product_model = ProductModel.new
+    @suppliers = Supplier.all
+  end
+
+  def create
+    product_model_params = params.require(:product_model).permit(:name, :height, :width, :depth, :weight, :sku,
+                                                                 :supplier_id)
+
+    @product_model = ProductModel.new(product_model_params)
+    if @product_model.save
+      redirect_to @product_model, notice: 'Modelo de produto cadastrado com sucesso'
+    else
+      flash.now[:alert] = 'Modelo de produto não cadastrado'
+      render 'new'
+    end
+  end
+
+  def show
+    @product_model = ProductModel.find(params[:id])
+  end
 end
