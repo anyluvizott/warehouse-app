fdf93baf8c840cace1b4a3ef683e7e57b150524f
Anyelly Marrye Luvizott <any.luvizott@gmail.com>
2022-09-26 22:35:00 -0300

    Lista dos Produtos dos Fornecedores

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,5 +1,8 @@
 class Supplier < ApplicationRecord
-  validates :corporate_name, :brand_name, :registration_number, :full_address, :city, :state, :email, :phone_number, presence: true
+  has_many :product_models
+
+  validates :corporate_name, :brand_name, :registration_number, :full_address, :city, :state, :email, :phone_number,
+            presence: true
   validates :registration_number, uniqueness: true
   validates :registration_number, cnpj: true
 end
