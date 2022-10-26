aed872c45bf957d5f08a37c24cbbe6071b271652
Anyelly Marrye Luvizott <any.luvizott@gmail.com>
2022-10-26 00:19:35 -0300

    Tratando Erros - criando teste endpoint create

--- spec/requests/apis/warehouse_api_spec.rb
+++ spec/requests/apis/warehouse_api_spec.rb
@@ -63,5 +63,61 @@ describe 'Warehouse API' do
       json_response = JSON.parse(response.body)
       expect(json_response).to eq []
     end
+
+    it 'and raise internal error' do
+      allow(Warehouse).to receive(:all).and_raise(ActiveRecord::QueryCanceled)
+
+      get '/api/v1/warehouses'
+
+      expect(response).to have_http_status(500)
+    end
+  end
+
+  context 'POST /api/v1/warehouses' do
+    it 'success' do
+      warehouse_params = { warehouse: { name: 'Aeroporto do Rio de Janeiro', code: 'SDU', city: 'Rio de Janeiro', area: 95_800,
+                                        address: 'Praça Sen. Salgado Filho, s/n - Centro', cep: '20021-340',
+                                        description: 'Galpão destinada para cargas da região do Rio de Janeiro' } }
+
+      post '/api/v1/warehouses', params: warehouse_params
+
+      expect(response).to have_http_status(201)
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response['name']).to eq 'Aeroporto do Rio de Janeiro'
+      expect(json_response['code']).to eq 'SDU'
+      expect(json_response['city']).to eq 'Rio de Janeiro'
+      expect(json_response['area']).to eq 95_800
+      expect(json_response['address']).to eq 'Praça Sen. Salgado Filho, s/n - Centro'
+      expect(json_response['cep']).to eq '20021-340'
+      expect(json_response['description']).to eq 'Galpão destinada para cargas da região do Rio de Janeiro'
+    end
+
+    it 'fail if parameters are not complete' do
+      warehouse_params = { warehouse: { name: 'Aeroporto Curitiba', code: 'CWB' } }
+
+      post '/api/v1/warehouses', params: warehouse_params
+
+      expect(response).to have_http_status(412)
+      expect(response.body).not_to include 'Nome não pode ficar em branco'
+      expect(response.body).not_to include 'Código não pode ficar em branco'
+      expect(response.body).to include 'Cidade não pode ficar em branco'
+      expect(response.body).to include 'Área não pode ficar em branco'
+      expect(response.body).to include 'Endereço não pode ficar em branco'
+      expect(response.body).to include 'CEP não pode ficar em branco'
+      expect(response.body).to include 'Descrição não pode ficar em branco'
+    end
+
+    it 'fail if theres an internal error' do
+      allow(Warehouse).to receive(:new).and_raise(ActiveRecord::ActiveRecordError)
+
+      warehouse_params = { warehouse: { name: 'Aeroporto do Rio de Janeiro', code: 'SDU', city: 'Rio de Janeiro', area: 95_800,
+                                        address: 'Praça Sen. Salgado Filho, s/n - Centro', cep: '20021-340',
+                                        description: 'Galpão destinada para cargas da região do Rio de Janeiro' } }
+
+      post '/api/v1/warehouses', params: warehouse_params
+
+      expect(response).to have_http_status(500)
+    end
   end
 end
